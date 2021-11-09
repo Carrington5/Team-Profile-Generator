bcc739299c12ae2f0c5ddb2afa29176ea34f9d7c
Carrington5 <88473030+Carrington5@users.noreply.github.com>
2021-11-08 23:37:07 -0600

    add inquire questions for the manager

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
 const inquirer = require('inquirer');
-const Engineer = require('./lib/Engineer')
+const Engineer = require('./lib/Engineer');
+const Intern   = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+const path = require('path');
+const fs = require('fs');
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
+const render = require('./src/page-template');
+
+const teamMembersArr = []
+const idArr = []
+
+function appCreate() {
+    function createManager() {
+        inquirer.prompt([
+            {
+                type:'input',
+                name:'mgrName',
+                message: "What is the team manager's name?"
+            },
+            {
+                type: 'input',
+                name: 'mgrId',
+                message: "What is the team manager's employee ID?"
+            },
+            {
+                type: 'input',
+                name: 'mgrEmail',
+                message: "What is the team manager's Email address?"
+            },
+            {
+                type: 'input',
+                name: 'mgrOffice',
+                message: "What is the team manager's office number?"
+            }
+        ])
+    }
+}
